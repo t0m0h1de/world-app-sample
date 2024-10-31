60bbdddd981d27cad632c9ba4f00abe93e4186e4
Tomohide Sawada <ma1n.tmrev01ut10n@gmail.com>
2024-11-01 00:45:54 +0900

    Update

--- main.go
+++ main.go
@@ -10,25 +10,25 @@ import (
 func main() {
 	db, err := sql.Open("postgres", "user=postgres password=postgres dbname=postgres port=5433 sslmode=disable")
 	errHandler(err)
+	city := "Ede"
+	population := findPopulation(db, city)
+	fmt.Printf("The population of %s: %d\n", city, population)
 
 	// データの検索
-	rows, err := db.Query("SELECT name, id, population FROM city")
-	errHandler(err)
+}
 
-	for rows.Next() {
-		var name string
-		var id int
-		var population int
-		err = rows.Scan(&name, &id, &population)
-		errHandler(err)
-		fmt.Println(name)
-		fmt.Println(id)
-		fmt.Println(population)
-	}
+func findPopulation(db *sql.DB, city string) int {
+	// データの検索
+	query := fmt.Sprintf("SELECT population FROM city WHERE name = '%s'", city)
+	var population int
+	err := db.QueryRow(query).Scan(&population)
+	errHandler(err)
+	return population
 }
 
 func errHandler(err error) {
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
 }
